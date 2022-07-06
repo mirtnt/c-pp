ac082e38f1540220e005083cd1904658864dd6a0
mirtnt <49514958+mirtnt@users.noreply.github.com>
2022-07-06 23:24:16 +0800

    Create test3.cpp

---
+++ 第二章/test3.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void tbm();
+void shtr();
+
+int main()
+{
+    tbm();
+    tbm();
+    shtr();
+    shtr();
+
+    //system("pause");//命令框暂停
+    return 0;
+}
+void tbm()
+{
+    cout<<"Three blind mice"<<endl;
+}
+void shtr()
+{
+    cout<<"See how they run"<<endl;
+}
