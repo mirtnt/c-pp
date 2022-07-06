0a105fe6ef750b489924a28c6a4a32d7e165c8df
mirtnt <49514958+mirtnt@users.noreply.github.com>
2022-07-06 23:26:00 +0800

    Create test4.cpp

---
+++ 第二章/test4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"输入你的年龄：";
+    int year;
+    cin>>year;
+    cout<<"你的月份是 "<<year*12;
+
+    //system("pause");//命令框暂停
+    return 0;
+}
